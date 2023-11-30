67655292fff253bce1de485258208494ee3aa9be
Eric Frederickson <ericfrederickson68@gmail.com>
2023-11-30 03:07:28 -0600

    split Surface:process into multiple functions to enable easy testing

--- lib/Surface.hs
+++ lib/Surface.hs
@@ -2,35 +2,48 @@ module Surface
   ( process
   ) where
 
+import Text.Parsec (ParseError)
+
 import Chords
 import Parser
 import Translation
 
+type Result = ([Note], [Int], [Match])
+
+getResult :: String -> Either ParseError Result
+getResult s =
+  case parseNotes s of
+    Left err -> Left err
+    Right notes ->
+      let intervals = notesToIntervals notes
+      in Right ( notes
+               , intervals
+               , allPossibleChords intervals)
+
+matchToStringContextual :: ([Note], [Int], Match) -> String
+matchToStringContextual (notes, intervals, match) =
+  case match of
+    ExactMatch _chord chordInv ->
+      getRootString notes chordInv
+      ++ " " ++ matchToString match
+    CloseMatch _chord chordInv ivTransformation ->
+      getRootString
+        (transformNotes notes (intervals, ivTransformation))
+        chordInv
+      ++ " " ++ matchToString match ++ " (close position)"
+  where
+    getRootString :: [Note] -> Int -> String
+    getRootString ns inv =
+      noteToString (ns !! ((-inv) `mod` length ns))
+
 process :: String -> [String]
 process s =
-  case parseNotes s of
+  case getResult s of
     Left err -> [show err]
-    Right notes ->
-      let
-        intervals = notesToIntervals notes
-      in
-      case
-        allPossibleChords intervals
-      of
+    Right (notes, intervals, matches) ->
+      case matches of
         [] -> ["Unknown chord"]
-        matches ->
-          map (\match ->
-                 let
-                   getRootString :: [Note] -> Int -> String
-                   getRootString ns inv =
-                     noteToString (ns !! ((-inv) `mod` length ns))
-                 in case match of
-                   ExactMatch _chord chordInv ->
-                     getRootString notes chordInv
-                     ++ " " ++ matchToString match
-                   CloseMatch _chord chordInv ivTransformation ->
-                     getRootString
-                       (transformNotes notes (intervals, ivTransformation))
-                       chordInv
-                     ++ " " ++ matchToString match ++ " (close position)"
-              ) matches
+        _ -> map (\ match ->
+                    matchToStringContextual
+                      (notes, intervals, match)
+                 ) matches
